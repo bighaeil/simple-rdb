abded0b69c9cf2e79101b9dfd5a339aecaf1809b
bighaeil <bighaeil@gmail.com>
2024-05-01 23:16:08 +0900

    init

---
+++ src/test/java/com/simple/mydb/MydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.mydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
